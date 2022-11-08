f18ee1506946b545f96d99d919fc5a934416033f
akojamil <akojamil93@gmail.com>
2022-11-08 14:02:53 -0500

    small modification to readout subdirs

--- sipm/dataset.py
+++ sipm/dataset.py
@@ -4,8 +4,9 @@ import yaml
 import sipm.sipm as sipm
 
 class Dataset: 
-    def __init__(self,  path, pol=1, channels=4, samples=3000):
-        self.path = path
+    def __init__(self,  path, pol=-1, channels=4, samples=3000):
+        self.datadir = "/scratch/gpfs/GALBIATI/data/sipm/reflector_studies/"
+        self.path = f"{self.datadir}/{path}"
         self.channels = channels
         self.samples = samples
         self.pol = pol
@@ -17,3 +18,21 @@ class Dataset:
             new_channel = sipm.SiPM(id=i, pol=self.pol, path=self.path, samples=self.samples)
             channels.append(new_channel)
         return channels
+    
+    def analyze(self, header=True, num_events=1e9):
+        for i in self.channels:
+            self.ch[i].read_data(header=header, num_events=num_events)
+            self.ch[i].bandpass_filter(low=1, high=8e6, order=1)
+            self.ch[i].baseline_subtraction()
+            self.ch[i].get_max()
+            self.ch[i].get_integral()
+    
+    def calibrate(self):
+        for i in self.channels:
+            self.ch[i].calibrate(self.ch[i].peak, width=10, verbose=1)
+            self.ch[i].plot_calibration(self.ch[i].peak)
+    
+    def clear(self):
+        for i in self.channels:
+            self.ch[i].clear()
+        
--- sipm/sipm.py
+++ sipm/sipm.py
@@ -1,13 +1,22 @@
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
+
 from scipy import signal
+from scipy.fft import fft
+from scipy.optimize import curve_fit
+
+import sipm.functions as func
+
+from BaselineRemoval import BaselineRemoval
+
 
 class SiPM():
     def __init__(self, id, pol, path, samples):
         self.path = path
         self.id = id
         self.pol = pol
-        self.file = glob.glob(self.path+"wave{}.dat".format(self.id))[0]
+        self.files = glob.glob(self.path+"wave{}.dat".format(self.id))
         self.sampling = 250000000 # in MHz
         self.sample_step = 1./float(self.sampling)*1e6 # in us
         self.traces = []
@@ -19,35 +28,38 @@ class SiPM():
         self.traces = []
         self.timestamp = []
     
-    def read_data(self, header=True):
-        file = open(self.file, 'rb')
-        if header:
-            for i in range(1000000):
-                self.header = np.fromfile(file, dtype=np.dtype('I'), count=6)
-                if len(self.header) == 0:
-                    break
-                self.samples = (self.header[0] - 24) // 2
-                self.timestamp.append(self.header[-1])
-                trace = np.fromfile(file, dtype=np.dtype('<H'), count=self.samples)
-                self.traces.append(trace)
-        else:
-            self.traces = np.fromfile(file, dtype=np.dtype('<H'), count=-1)
-        
-        file.close()
+    def read_data(self, header=True, num_events=1e9):
+        for f in sorted(self.files):
+            file = open(f, 'rb')
+            if header:
+                for i in range(1000000):
+                    self.header = np.fromfile(file, dtype=np.dtype('I'), count=6)
+                    if len(self.header) == 0 or i>num_events:
+                        break
+                    self.samples = (self.header[0] - 24) // 2
+                    self.timestamp.append(self.header[-1])
+                    trace = np.fromfile(file, dtype=np.dtype('<H'), count=self.samples)
+                    self.traces.append(trace)
+            else:
+                self.traces = np.fromfile(file, dtype=np.dtype('<H'), count=-1)
+            file.close()
+
         self.traces = np.array(self.traces)
         self.traces = self.traces.reshape((-1,self.samples)).astype(float)
         self.time = np.arange(0,self.sample_step*self.samples,self.sample_step)
 
-    def baseline_subtraction(self):
+    def baseline_subtraction(self, samples=500):
         for ii,x in enumerate(self.traces):
-            baseline = np.mean(self.traces[ii][:500])
+            baseline = np.mean(self.traces[ii][:samples])
             self.traces[ii] -= baseline
             self.traces[ii] *= self.pol
 
-    def bandpass_filter(self, low, high, order=3):
+    def bandpass_filter(self, low, high, order=3, keep=False):
         if not self.filt_pars:
             b, a = signal.butter(order, [low,high], analog=False, fs=self.sampling, btype='band')
             self.filt_pars = [b,a]
+        if keep:
+            self.traces_orig = self.traces.copy()
         for ii,x in enumerate(self.traces):
             self.traces[ii] = signal.filtfilt(*self.filt_pars, x)
     
@@ -61,9 +73,84 @@ class SiPM():
     def get_integral(self):
         self.integral = []
         for ii,x in enumerate(self.traces):
-            self.integral.append(np.sum(x[self.peak_pos[ii]-50:self.peak_pos[ii]+500]))
+            self.integral.append(np.sum(x[1500:]))
             # self.integral.append(np.sum(x[self.peak_pos[ii]-50:]))
             # self.integral.append(np.sum(x))
+
+    def rolling_baseline(self):
+        return 0
     
+    def get_rolling_integral(self):
+        self.rolling_integral = []
+        for x in self.traces:
+            trace_corr = BaselineRemoval(x)
+            self.rolling_integral.append(np.sum(trace_corr.IModPoly(10)))
+        self.rolling_integral = np.array(self.rolling_integral)
+
+    def get_fft(self):
+        self.fft = []
+        for ii,x in enumerate(self.traces):
+            y = fft(x)
+            y = 2.0/self.samples * np.abs(y[1:self.samples//2])
+            self.fft.append(y)
+            
     def clear(self):
         self.traces = []
+
+    def calibrate(self,vals,width=6,prominence=2,verbose=-1,fitrange=10):
+        self.fit_p = []
+        self.fit_c = []
+
+        h,hx = np.histogram(vals, bins=np.arange(0,np.max(vals),.1))
+        self.pp = []
+        while len(self.pp)<=3:
+            print('Peak search with width={} ...'.format(width))
+            self.pp,self.pdict = signal.find_peaks(h, prominence=prominence, width=width)
+            width -= 1
+
+        if verbose > 0:
+            print("Found {} peaks".format(len(self.pp)))
+
+        for x in self.pp: 
+            fit_x = hx[:-1][x-fitrange:x+fitrange]
+            fit_y = h[x-fitrange:x+fitrange]
+            popt,pcov = curve_fit(func.gauss, fit_x, fit_y, p0=[h[x], hx[:-1][x], 2], maxfev=10000)
+            self.fit_p.append(popt)
+            self.fit_c.append(pcov)
+
+        # quick estimate of gain
+        gain = np.median(np.diff(np.array(self.fit_p)[:,1]))
+        # get peak number
+        peak_num = np.round(np.array(self.fit_p)[:,1]/gain)
+
+        self.calib,self.calib_err = curve_fit(func.line, peak_num, np.array(self.fit_p)[:,1])
+        self.calib_err = np.sqrt(np.diag(self.calib_err))
+
+    def get_breakdown(self):
+        return 0 
+
+        
+    def plot_calibration(self,vals):
+        fig, ax = plt.subplots(figsize=(9,3), ncols=2,nrows=1)
+        h,hx = np.histogram(vals, bins=np.arange(0,np.max(vals),.1))
+        ax[0].step(hx[:-1],h, where='post')
+        ax[0].scatter(hx[self.pp], h[self.pp], color='r', s=3, zorder=10)
+        ax[0].set_xlabel('Amplitude [mV]')
+        ax[0].set_ylabel('Counts')
+        ax[0].set_yscale('log')
+        ax[0].set_xlim(0,np.max(hx[self.pp])+20)
+        ax[0].set_ylim(1e0,1e4)
+
+        # quick estimate of gain
+        gain = np.median(np.diff(np.array(self.fit_p)[:,1]))
+        # get peak number
+        peak_num = np.round(np.array(self.fit_p)[:,1]/gain)
+
+        xfit = np.linspace(0,20,100)
+
+        ax[1].grid()
+        ax[1].set_ylim(0,50)
+        ax[1].scatter(peak_num, np.array(self.fit_p)[:,1], marker='o', s=20, zorder=10)
+        ax[1].plot(xfit, func.line(xfit, *self.calib), ls='--', label=r'$m=({:.3f}\pm {:.3f})$/p.e.'.format(self.calib[0], self.calib_err[0]))
+        ax[1].legend(loc='lower right')
+        plt.show()
