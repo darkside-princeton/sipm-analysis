5118e26a14384d853d86dec2005836c778bf23c9
allan-ylsung <as111@princeton.edu>
2022-11-22 17:24:40 -0500

    Updated for 11/22 data

--- script/root_calibration.py
+++ script/root_calibration.py
@@ -10,32 +10,32 @@ if __name__ == '__main__':
     #usage: python root_calibration.py index
     index = int(sys.argv[1])
     data_dir = '/scratch/gpfs/GALBIATI/data/sipm/reflector_studies/'
-    direc = [   '2022-11-01/2022-11-01_volt_61_pos_top_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_63_pos_top_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_65_pos_top_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_67_pos_top_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_69_pos_top_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_61_pos_bottom_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_63_pos_bottom_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_65_pos_bottom_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_67_pos_bottom_light_laser_coinc_none_cond_calibration',
-                '2022-11-01/2022-11-01_volt_69_pos_bottom_light_laser_coinc_none_cond_calibration']
-    nsubdir = [1,1,1,1,1,1,1,1,1,1]
-    outfile = [ 'calibration_61V_top',
-                'calibration_63V_top',
-                'calibration_65V_top',
-                'calibration_67V_top',
-                'calibration_69V_top',
-                'calibration_61V_bottom',
-                'calibration_63V_bottom',
-                'calibration_65V_bottom',
-                'calibration_67V_bottom',
-                'calibration_69V_bottom']
+    direc = [   '2022-11-22/2022-11-22_volt_63_pos_top_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_65_pos_top_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_67_pos_top_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_69_pos_top_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_71_pos_top_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_63_pos_bottom_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_65_pos_bottom_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_67_pos_bottom_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_69_pos_bottom_light_laser_coinc_laser_cond_calibration/',
+                '2022-11-22/2022-11-22_volt_71_pos_bottom_light_laser_coinc_laser_cond_calibration/']
+    nsubdir = [20]*10
+    outfile = [ 'calibration_1122_63V_top',
+                'calibration_1122_65V_top',
+                'calibration_1122_67V_top',
+                'calibration_1122_69V_top',
+                'calibration_1122_71V_top',
+                'calibration_1122_63V_bottom',
+                'calibration_1122_65V_bottom',
+                'calibration_1122_67V_bottom',
+                'calibration_1122_69V_bottom',
+                'calibration_1122_71V_bottom'
+            ]
 
-    data = ds.Dataset('', mode='calibration', pol=-1, channels=range(4), root_file_name='{}.root'.format(outfile[index]))
+    data = ds.Dataset('', mode='calibration', pol=-1, channels=range(4), root_file_name='../root/{}.root'.format(outfile[index]))
     for i in range(nsubdir[index]):
-        # subdir = '{}{}/'.format(direc, i)
-        subdir = '{}{}/'.format(data_dir, direc[index])
+        subdir = '{}{}{}/'.format(data_dir, direc[index],i)
         print(subdir)
         for ch in range(4):    
             data.ch[ch].path = subdir
