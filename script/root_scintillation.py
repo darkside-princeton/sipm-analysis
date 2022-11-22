3bd8d655748853c8c1bfacc737754989516b2817
allan-ylsung <as111@princeton.edu>
2022-11-22 01:02:54 -0500

    Slurm script for reading scintillation data

--- script/root_scintillation.py
+++ script/root_scintillation.py
@@ -6,23 +6,87 @@ import ROOT
 import sys
 import os
 from array import array
+import csv
 
 if __name__ == '__main__':
-    #usage: python root_scintillation.py <data directory> <number of subdirectories> <output file name> <calibration file>
-    direc = sys.argv[1]
-    nsubdir = int(sys.argv[2])
-    outfile = sys.argv[3]
-    calib_file = sys.argv[4]
+    #usage: python root_calibration.py index
+    index = int(sys.argv[1])
+    data_dir = '/scratch/gpfs/GALBIATI/data/sipm/reflector_studies/'
+    direc = [   '2022-11-20/2022-11-20_volt_63_pos_top_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_65_pos_top_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_67_pos_top_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_69_pos_top_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_71_pos_top_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_63_pos_bottom_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_65_pos_bottom_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_67_pos_bottom_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_69_pos_bottom_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-20/2022-11-20_volt_71_pos_bottom_light_scintillation_coinc_000_cond_gamma/',
+                '2022-11-18/2022-11-18_volt_63_pos_top_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_65_pos_top_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_67_pos_top_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_69_pos_top_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_71_pos_top_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_63_pos_bottom_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_65_pos_bottom_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_67_pos_bottom_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_69_pos_bottom_light_scintillation_coinc_000_cond_no_gamma/',
+                '2022-11-18/2022-11-18_volt_71_pos_bottom_light_scintillation_coinc_000_cond_no_gamma/'
+            ]
+    nsubdir = [20]*20
+    outfile = [ 'gamma_63V_top',
+                'gamma_65V_top',
+                'gamma_67V_top',
+                'gamma_69V_top',
+                'gamma_71V_top',
+                'gamma_63V_bottom',
+                'gamma_65V_bottom',
+                'gamma_67V_bottom',
+                'gamma_69V_bottom',
+                'gamma_71V_bottom',
+                'backgrounds_63V_top',
+                'backgrounds_65V_top',
+                'backgrounds_67V_top',
+                'backgrounds_69V_top',
+                'backgrounds_71V_top',
+                'backgrounds_63V_bottom',
+                'backgrounds_65V_bottom',
+                'backgrounds_67V_bottom',
+                'backgrounds_69V_bottom',
+                'backgrounds_71V_bottom'
+            ]
     ##########
     # SPE gain is preliminary. Should use Qavg/(1-p) obtained from calibration analysis!
     ##########
     ##########
     # Read Qavg/(1-p) from calibration file
     ##########
-    spe_gain = [550,550,550,550] 
-    data = ds.Dataset('', mode='scintillation', spe=spe_gain, pol=-1, channels=range(4), root_file_name='{}.root'.format(outfile))
-    for i in range(nsubdir):
-        subdir = '{}{}/'.format(direc, i)
+    bias = [63,65,67,69,71]
+    volt = bias[index%5]
+    spe_gain = []
+    try:
+        with open('../calibration_{}V.csv'.format(volt)) as f:
+            r = csv.reader(f)
+            line_count = 0
+            for row in r:
+                if line_count>0:
+                    if index%10<5:
+                        if line_count<=4:
+                            spe_gain.append(float(row[5])/(1-float(row[3])))
+                    else:
+                        if line_count>4:
+                            spe_gain.append(float(row[5])/(1-float(row[3])))
+                line_count += 1
+    except:
+        spe_gain = [550]*4
+        print('No calibration csv file. Use default spe_gain of 550.')
+
+    for ch in range(4):
+        print('Ch{} spe_gain=[{:.3f},{:.3f},{:.3f},{:.3f}]'.format(ch, *spe_gain))
+
+    data = ds.Dataset('', mode='scintillation', spe=spe_gain, pol=-1, channels=range(4), root_file_name='{}.root'.format(outfile[index]))
+    for i in range(nsubdir[index]):
+        subdir = '{}{}{}/'.format(data_dir, direc[index], i)
         print(subdir)
         for ch in range(4):    
             data.ch[ch].path = subdir
