e699db1667064b7f1a1357f3b81d28c5b4d5d039
allan-ylsung <as111@princeton.edu>
2023-05-30 19:24:54 -0400

    hdf5 I/O

--- sipm/analysis/AdvancedAnalyzer.py
+++ sipm/analysis/AdvancedAnalyzer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict
 import glob
 import pandas as pd
 import yaml
@@ -6,7 +6,7 @@ import yaml
 class AdvancedAnalyzer:
     """The parent class for the post-analysis code.
     """
-    def __init__(self, dir:str,voltages:List[float], channels:List[int], positions:List[str]):
+    def __init__(self, directory:str, metadata_dict:Dict, wf:bool, merge:bool, verbose:bool):
         """Constructor.
 
         Args:
@@ -15,19 +15,43 @@ class AdvancedAnalyzer:
             channels (List[int]): Channel number (e.g. [0,1,2,3]) \n
             positions (List[str]): Position (e.g. ['top','bottom'])
         """
-        self.directory = dir
+        self.directory = directory
+        self.metadata = metadata_dict
         self.data = {}
         self.proto_data = {}
-        self.results = {}
-        self.voltages = voltages
-        self.channels = channels
-        self.positions = positions
+        self.results = None
+        self.wf = wf
+        self.merge = merge
+        self.verbose = verbose
 
     def print_info(self):
-        print('Data structure:\n','level1=position','level2=channel','level3=voltage')
+        print('\nData structure:')
         print(yaml.dump(self.proto_data,default_flow_style=False))
 
-    def load_files(self, date:str, light:str, coinc:str, cond:str, run:str, wf:bool, verbose:bool):
+    def load_file(self, date:str, volt:float, pos:str, light:str, coinc:str, cond:str, run:int, ch:int):
+        data = []
+        if self.wf:
+            files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}_wf.h5")
+        else:
+            files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}[!_wf].h5")
+        for f in files:
+            df = pd.read_hdf(f, key=f'{volt}/{ch}')
+            data.append(df)
+        if len(files)>0 and self.merge:
+            data = pd.concat(data, ignore_index=True)
+        if self.verbose:
+            if len(files)==0:
+                events = 0
+            else:
+                keys = data.keys()
+                events = len(data[keys[0]])
+            if self.wf:
+                print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} ch{ch} wf - {len(files)} files {events} events')
+            else:
+                print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} ch{ch} - {len(files)} files {events} events')
+        return data
+
+    def load_files(self):
         """Load pre-processed hdf5 files. Arguments need to match the file name substrings. Can use '*' for pattern matching except for the 'wf' argument.
 
         Args:
@@ -39,34 +63,18 @@ class AdvancedAnalyzer:
             wf (bool): Processed by laser_pulse.py or scintillation_pulse.py = True; processed by laser_waveform.py or scintillation_waveform.py = False. \n
             verbose (bool): Print messages \n
         """
-        for pos in self.positions:
-            self.data[pos] = {}
-            self.proto_data[pos] = {}
-            for ch in self.channels:
-                self.data[pos][ch] = {}
-                self.proto_data[pos][ch] = {}
-                for volt in self.voltages:
-                    self.data[pos][ch][volt] = []
-                    self.proto_data[pos][ch][volt] = {}
-                    if wf:
-                        files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}_wf.h5")
-                    else:
-                        files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}[!_wf].h5")
-                    for f in files:
-                        df = pd.read_hdf(f, key=f'{volt}/{ch}')
-                        self.data[pos][ch][volt].append(df)
-                    if len(files)>0:
-                        self.data[pos][ch][volt] = pd.concat(
-                            self.data[pos][ch][volt], ignore_index=True)
-                    for k in self.data[pos][ch][volt].keys():
-                        self.proto_data[pos][ch][volt][k] = []
-                    if verbose and ch==self.channels[0]:
-                        if len(files)==0:
-                            events = 0
-                        else:
-                            keys = self.data[pos][ch][volt].keys()
-                            events = len(self.data[pos][ch][volt][keys[0]])
-                        if wf:
-                            print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} wf - {len(files)} files {events} events')
-                        else:
-                            print(f'{date} {volt} {pos} {light} coinc={coinc} cond={cond} run{run} - {len(files)} files {events} events')
+        self.metadata_dfs(self.metadata, self.data, self.proto_data)
+        if self.verbose:
+            self.print_info()
+
+    def metadata_dfs(self, mydict, mydata, myproto):
+        if 'metadata' not in mydict:
+            for k,v in mydict.items():
+                mydata[k] = {}
+                myproto[k] = {}
+                self.metadata_dfs(v,mydata[k],myproto[k])
+        else:
+            m = mydict['metadata']
+            mydata['data'] = self.load_file(m['date'],m['volt'],m['pos'],m['light'],m['coinc'],m['cond'],m['run'],m['ch'])
+            for k in mydata['data'].keys():
+                myproto[k] = '###'
--- sipm/analysis/SipmCalibration.py
+++ sipm/analysis/SipmCalibration.py
@@ -1,5 +1,10 @@
 from sipm.analysis.AdvancedAnalyzer import AdvancedAnalyzer
+from typing import Dict, List
+
 
 class SipmCalibration(AdvancedAnalyzer):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, positions:List[str], channels:List[int], voltages:List[float], directory:str, metadata_dict:Dict, wf:bool, merge:bool, verbose:bool):
+        super().__init__(directory, metadata_dict, wf, merge, verbose)
+        self.positions = positions
+        self.channels = channels
+        self.voltages = voltages
