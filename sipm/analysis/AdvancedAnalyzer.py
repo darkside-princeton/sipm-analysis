7c72be4674c8ef272c6033c60222c1b00d0397f7
allan-ylsung <as111@princeton.edu>
2023-05-30 19:24:54 -0400

    hdf5 I/O

--- sipm/analysis/AdvancedAnalyzer.py
+++ sipm/analysis/AdvancedAnalyzer.py
@@ -7,8 +7,8 @@ import numpy as np
 class AdvancedAnalyzer:
     """The parent class for the post-analysis classes.
     """
-    def __init__(self, directory:str, metadata_dict:Dict, script:str, merge:bool, verbose:bool):
-        """AdvancedAnalyzer constructor.
+    def __init__(self, directory:str, metadata_dict:Dict, wf:bool, merge:bool, verbose:bool):
+        """Constructor.
 
         Args:
             directory (str): Directory containing processed HDF5 files (e.g. '/scratch/gpfs/as111/results/')
@@ -21,19 +21,40 @@ class AdvancedAnalyzer:
         self.metadata = metadata_dict
         self.data = {}
         self.proto_data = {}
-        self.script = script
+        self.results = None
+        self.wf = wf
         self.merge = merge
         self.verbose = verbose
-        self.baseline = {}
 
     def print_info(self):
-        """Print nested structure of self.data with every lowest-level entry replaced by '###'.
-        """
         print('\nData structure:')
         print(yaml.dump(self.proto_data,default_flow_style=False))
 
     def load_file(self, date:str, volt:float, pos:str, light:str, coinc:str, cond:str, run:int, ch:int):
-        """Load the file specified by the metadata in the arguments. Can use '*' for pattern matching.
+        data = []
+        if self.wf:
+            files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}_wf.h5")
+        else:
+            files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}[!_wf].h5")
+        for f in files:
+            df = pd.read_hdf(f, key=f'{volt}/{ch}')
+            data.append(df)
+        if len(files)>0 and self.merge:
+            data = pd.concat(data, ignore_index=True)
+        if self.verbose:
+            if len(files)==0:
+                events = 0
+            else:
+                keys = data.keys()
+                events = len(data[keys[0]])
+            if self.wf:
+                print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} ch{ch} wf - {len(files)} files {events} events')
+            else:
+                print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} ch{ch} - {len(files)} files {events} events')
+        return data
+
+    def load_files(self):
+        """Load pre-processed hdf5 files. Arguments need to match the file name substrings. Can use '*' for pattern matching except for the 'wf' argument.
 
         Args:
             date (str): Date (e.g. '2022-11-22')
@@ -48,41 +69,11 @@ class AdvancedAnalyzer:
         Returns:
             pandas.DataFrame: data
         """
-        data = []
-        files = glob.glob(f"{self.directory}{date}/{date}_volt_{volt}_pos_{pos}_light_{light}_coinc_{coinc}_cond_{cond}_run{run}_{self.script}.h5")
-        for f in files:
-            ind = f.find('run')+3
-            run_number = int(f[ind:f.find('_',ind)])
-            df = pd.read_hdf(f, key=f'{volt}/{ch}')
-            df['event'] = df.index
-            df['run'] = np.array([run_number]*df.shape[0])
-            data.append(df)
-        if len(files)>0 and self.merge:
-            data = pd.concat(data, ignore_index=True).sort_values(by=['run','event']).reset_index(drop=True)
-        if self.verbose:
-            if len(files)==0:
-                events = 0
-            else:
-                keys = data.keys()
-                events = len(data[keys[0]])
-            print(f'{date} {volt}V {pos} {light} coinc={coinc} cond={cond} run{run} ch{ch} script={self.script}.py - {len(files)} files {events} events')
-        return data
-
-    def load_files(self):
-        """Load processed HDF5 files specified by self.metadata.
-        """
         self.metadata_dfs(self.metadata, self.data, self.proto_data)
         if self.verbose:
             self.print_info()
 
-    def metadata_dfs(self, mydict:Dict, mydata:Dict, myproto:Dict):
-        """Loop over and load all files recursively.
-
-        Args:
-            mydict (Dict): metadata dictionary
-            mydata (Dict): data dictionary
-            myproto (Dict): proto-data dictionary (same structure as data but with data content replaced by '###')
-        """
+    def metadata_dfs(self, mydict, mydata, myproto):
         if 'metadata' not in mydict:
             for k,v in mydict.items():
                 mydata[k] = {}
@@ -90,39 +81,6 @@ class AdvancedAnalyzer:
                 self.metadata_dfs(v,mydata[k],myproto[k])
         else:
             m = mydict['metadata']
-            mydata['metadata'] = mydict['metadata']
             mydata['data'] = self.load_file(m['date'],m['volt'],m['pos'],m['light'],m['coinc'],m['cond'],m['run'],m['ch'])
             for k in mydata['data'].keys():
                 myproto[k] = '###'
-                
-    def baseline_analysis(self, threshold_dict:Dict):
-        """Apply baseline quality cuts and calculate cut fraction.
-
-        Args:
-            threshold_dict (Dict): Baseline rms thresholds arranged into a nested dictionary with the same structure as self.metadata.
-        """
-        self.bsl_rms_thre = threshold_dict
-        self.baseline_cut_dfs(threshold_dict, self.data, self.baseline)
-    
-    def baseline_cut_dfs(self, mythre:Dict, mydata:Dict, mybsl:Dict):
-        """Loop over all data entry and apply baseline rms cut.
-
-        Args:
-            mythre (Dict): Baseline rms thresholds arranged into a nested dictionary with the same structure as self.metadata.
-            mydata (Dict): Data dictionary
-            mybsl (Dict): Baseline dictionary. Contains baseline rms and baseline mean distributions as well as cut fractions.
-        """
-        if 'data' not in mydata:
-            for k,v in mydata.items():
-                mybsl[k] = {}
-                self.baseline_cut_dfs(mythre[k],v,mybsl[k])
-        else:
-            data = mydata['data']
-            data['bsl_cut'] = data['baseline_rms']<mythre
-            mybsl['rms_threshold'] = mythre
-            mybsl['rms_hist'], mybsl['rms_bins'] = np.histogram(data['baseline_rms'], bins=500, range=(0, 5))
-            mybsl['mean_hist'], mybsl['mean_bins'] = np.histogram(data['baseline_mean'], bins=1500, range=(3720, 3850))
-            mybsl['mean_hist_cut'], mybsl['mean_bins_cut'] = np.histogram(data['baseline_mean'].loc[data['bsl_cut']], bins=1500, range=(3720, 3850))
-            mybsl['cut_fraction'] = 1-np.sum(data['bsl_cut'])/data.shape[0]
-            if self.verbose:
-                print(f'{mydata["metadata"]["pos"]} ch{mydata["metadata"]["ch"]} {mydata["metadata"]["volt"]}V cut fraction = {mybsl["cut_fraction"]*100:.5f}%')
