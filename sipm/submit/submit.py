453979cee308a4932d2b93af38c0b324b75b5984
allan-ylsung <as111@princeton.edu>
2023-06-30 09:37:54 -0400

    Modified submit.py to take any new config

--- sipm/submit/submit.py
+++ sipm/submit/submit.py
@@ -17,15 +17,17 @@ for _ in conf['files']:
 
 scheduler = scheduler.Scheduler(dirs=dirs)
 
-if conf['script'] == 'sipm/exe/analysis.py':
-    scheduler.submit(script=conf['script'],args=f"-n {conf['num_events']}")
-elif conf['script'] == 'sipm/exe/laser_pulse.py':
-    scheduler.submit(script=conf['script'],args=f"-n {conf['num_events']}")
-elif conf['script'] == 'sipm/exe/scintillation_pulse.py':
-    scheduler.submit(script=conf['script'],args=f"-n {conf['num_events']} -s {conf['calibration']}")
-elif conf['script'] == 'sipm/exe/laser_waveform.py' or conf['script']=='sipm/exe/fft.py':
-    scheduler.submit(script=conf['script'],args=f"-n {conf['num_events']} -s {conf['calibration']}")
-elif conf['script'] == 'sipm/exe/scintillation_waveform.py':
-    if 'cpu_memory' in conf:
-        scheduler.cpu_memory=conf['cpu_memory']
-    scheduler.submit(script=conf['script'],args=f"-n {conf['num_events']} -s {conf['calibration']} -p {conf['fprompt_range'][0]} {conf['fprompt_range'][1]} -e {conf['pe_range'][0]} {conf['pe_range'][1]}")
+if 'cpu_memory' in conf:
+    scheduler.cpu_memory=conf['cpu_memory']
+
+submit_string = ''
+for k,v in conf['arguments'].items():
+    submit_string += f' --{k}'
+    if isinstance(v,list):
+        for v_ in v:
+            submit_string += f' {v_}'
+    else:
+        submit_string += f' {v}'
+submit_string = submit_string[1:]
+
+scheduler.submit(script=conf['script'],args=submit_string)
