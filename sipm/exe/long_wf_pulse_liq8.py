7898728ba012ec502d085597ea7204a10b9f4ec5
allan-ylsung <as111@princeton.edu>
2025-03-06 11:46:37 -0500

    laser data 0304

--- sipm/exe/long_wf_pulse_liq8.py
+++ sipm/exe/long_wf_pulse_liq8.py
@@ -11,7 +11,7 @@ args = parser.parse_args()
 
 def main():
     # Create new dataset object
-    d = wfd.WaveformDataset(path=args.file_dir,channels=[0,1,2,3,4,7],trig=9960)
+    d = wfd.WaveformDataset(path=args.file_dir,channels=[0,1,2,3,4,5,6,7],trig=24950)
 
     # Run pulse analysis on scintillation data
 
